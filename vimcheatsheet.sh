f11de72fbf21a51930ae9b79aecec56476920f3d
gprogrammin <87392072+NothingButProgrammer@users.noreply.github.com>
2021-11-05 19:56:59 +0200

    Add files via upload
    1.0 version

---
+++ vimcheatsheet.sh
@@ -0,0 +1,25 @@
+############vim cheat sheet#############
+#########made by gprogrammin############
+##########fork me on github#############
+
+echo "____   ____.___    _____               .__                      __               .__                     __    
+\   \ /   /|   |  /     \        ____  |  |__    ____  _____  _/  |_       ______|  |__    ____   ____ _/  |_  
+ \   Y   / |   | /  \ /  \     _/ ___\ |  |  \ _/ __ \ \__  \ \   __\     /  ___/|  |  \ _/ __ \_/ __ \\   __\ 
+  \     /  |   |/    Y    \    \  \___ |   Y  \\  ___/  / __ \_|  |       \___ \ |   Y  \\  ___/\  ___/ |  |   
+   \___/   |___|\____|__  /     \___  >|___|  / \___  >(____  /|__|      /____  >|___|  / \___  >\___  >|__|   
+                        \/          \/      \/      \/      \/                \/      \/      \/     \/        
+                                                                                                               "
+
+echo "Vim cheat sheet, made by gprogrammin on github."
+echo "[PRESS ANY KEY TO INSTALL VIM]"
+read vim
+sudo apt install vim
+echo "Vim installed!"
+echo "[PRESS ANY KEY TO START THE TUTORIAL]"
+read tutorial
+echo "start vim - vim file_name.txt"
+echo "insert mode - i"
+echo "view mode - ESC key"
+echo "exit - :q"
+echo "exit and save - :wq"
+
